2e0fc8bb03c9289d3299aa379a610bd8386f0b45
Farbod Shahinfar <fshahinfar1@gmail.com>
2026-01-29 10:00:46 -0700

    explore ideas

--- scripts/measure_cache_miss.sh
+++ scripts/measure_cache_miss.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+VENDOR=$(lscpu | grep Vendor | cut -d : -f 2 | tr -d ' ' | tr -d "\t")
+if [ $VENDOR != "AuthenticAMD" ]; then
+	echo "This script was tested on AMD. It might not work... check inside"
+	exit 1
+fi
+
 CORE=$1
 sudo perf stat -C $CORE \
 	-e cpu-clock \
@@ -11,4 +18,8 @@ sudo perf stat -C $CORE \
 	-e branches \
 	-e branch-misses \
 	-e page-faults \
+	-e stalled-cycles-frontend \
+	-e ex_ret_cops \
+	-e de_dis_uop_queue_empty_di0 \
 	-r 3  -- sleep 1
+
